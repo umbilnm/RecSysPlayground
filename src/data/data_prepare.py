debae08447ebc36c2015a64d98ba3df7e8b11537
Kirill <kill4ik2002@gmail.com>
2024-09-26 23:44:50 +0300

    black

--- src/data/data_prepare.py
+++ src/data/data_prepare.py
@@ -14,70 +14,88 @@ def cli():
     """Group of commands for data preparation."""
     pass
 
+
 @cli.command()
-@click.argument('input', type=click.Path(exists=True))
-@click.argument('train', type=click.Path())
-@click.argument('test', type=click.Path())
+@click.argument("input", type=click.Path(exists=True))
+@click.argument("train", type=click.Path())
+@click.argument("test", type=click.Path())
 def split_data(input, train, test):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """Runs data processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('splitting data by time')
+    logger.info("splitting data by time")
     data = pd.read_csv(input, parse_dates=["last_watch_dt"])
     gap = pd.Timedelta(days=2)
-    start_test_date = data['last_watch_dt'].max() - pd.Timedelta(days=7)
-    end_test_date = data['last_watch_dt'].max()
-    start_train_date = data['last_watch_dt'].max() - pd.Timedelta(days=14) - gap
-    end_train_date = data['last_watch_dt'].max() - gap - pd.Timedelta(days=7)
+    start_test_date = data["last_watch_dt"].max() - pd.Timedelta(days=7)
+    end_test_date = data["last_watch_dt"].max()
+    start_train_date = data["last_watch_dt"].max() - pd.Timedelta(days=14) - gap
+    end_train_date = data["last_watch_dt"].max() - gap - pd.Timedelta(days=7)
 
-    ## splitting 
-    train_sample = data[(data['last_watch_dt'] > start_train_date) & (data['last_watch_dt'] < end_train_date)]
-    test_sample = data[(data['last_watch_dt'] > start_test_date) & (data['last_watch_dt'] < end_test_date)]
+    # splitting
+    train_sample = data[
+        (data["last_watch_dt"] > start_train_date) & (data["last_watch_dt"] < end_train_date)
+    ]
+    test_sample = data[
+        (data["last_watch_dt"] > start_test_date) & (data["last_watch_dt"] < end_test_date)
+    ]
 
-    ## saving     
+    # saving
     train_sample.to_csv(train, index=False)
     test_sample.to_csv(test, index=False)
 
 
 @cli.command()
-@click.argument('input_path', type=click.Path(exists=True))
-@click.argument('output_folder', type=click.Path())
-def get_coo_matrix(input_path: str, output_folder: str, user_col='user_id', item_col='item_id', weight_col=None):
+@click.argument("input_path", type=click.Path(exists=True))
+@click.argument("output_folder", type=click.Path())
+def get_coo_matrix(
+    input_path: str,
+    output_folder: str,
+    user_col="user_id",
+    item_col="item_id",
+    weight_col=None,
+):
     train_sample = pd.read_csv(input_path)
     logger = logging.getLogger(__name__)
-    logger.info('prepare interactions coo_matrix')
+    logger.info("prepare interactions coo_matrix")
 
-    users_inv_mapping = {index: int(value) for index, value in enumerate(train_sample['user_id'].unique())}
+    users_inv_mapping = {
+        index: int(value) for index, value in enumerate(train_sample["user_id"].unique())
+    }
 
-    users_mapping = {v: k for k, v in users_inv_mapping.items()}
-    items_inv_mapping = {index: int(value) for index, value in enumerate(train_sample['item_id'].unique())}
-    items_mapping = {v: k for k, v in items_inv_mapping.items()}
+    users_mapping = {v: int(k) for k, v in users_inv_mapping.items()}
+    items_inv_mapping = {
+        index: int(value) for index, value in enumerate(train_sample["item_id"].unique())
+    }
+    items_mapping = {v: int(k) for k, v in items_inv_mapping.items()}
     if weight_col is None:
         weights = np.ones(len(train_sample), dtype=np.float32)
     else:
         weights = train_sample[weight_col].astype(np.float32)
 
-    interaction_matrix = sp.coo_matrix((
-    weights, 
-    (
-        train_sample[user_col].map(users_mapping.get), 
-        train_sample[item_col].map(items_mapping.get)
+    interaction_matrix = sp.coo_matrix(
+        (
+            weights,
+            (
+                train_sample[user_col].map(users_mapping.get),
+                train_sample[item_col].map(items_mapping.get),
+            ),
+        )
     )
-))  
     if not os.path.exists(output_folder):
         os.mkdir(output_folder)
-    
+
     sp.save_npz(os.path.join(output_folder, "interactions_matrix.npz"), interaction_matrix)
-    with open(os.path.join(output_folder, "users_mapping.json"), 'w') as f:
+
+    with open(os.path.join(output_folder, "users_mapping.json"), "w") as f:
         json.dump(users_mapping, f)
 
-    with open(os.path.join(output_folder, "items_inv_mapping.json"), 'w') as f:
+    with open(os.path.join(output_folder, "items_inv_mapping.json"), "w") as f:
         json.dump(items_inv_mapping, f)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     project_dir = Path(__file__).resolve().parents[2]
--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -6,42 +6,45 @@ import json
 import pandas as pd
 import scipy.sparse as sp
 from implicit.nearest_neighbours import CosineRecommender
-from utils import generate_implicit_recs_mapper
+from .utils import generate_implicit_recs_mapper
+
 
 @click.command()
-@click.argument('data_path', type=click.Path(exists=True))
-@click.argument('model_path', type=click.Path(exists=True))
-@click.argument('path_to_save')
+@click.argument("data_path", type=click.Path(exists=True))
+@click.argument("model_path", type=click.Path(exists=True))
+@click.argument("path_to_save")
 def inference_implicit_model(data_path: str, model_path: str, path_to_save: str) -> None:
-	logger = logging.getLogger(__name__)
-	model = CosineRecommender(K=20)
-	model = model.load(model_path)
-	logger.info(f'model loaded from {model_path}')
-	interactions_train_matrix = sp.load_npz(os.path.join(data_path, 'interactions_matrix.npz')) 
+    logger = logging.getLogger(__name__)
+    model = CosineRecommender(K=20)
+    model = model.load(model_path)
+    logger.info(f"model loaded from {model_path}")
+    interactions_train_matrix = sp.load_npz(os.path.join(data_path, "interactions_matrix.npz"))
 
-	with open(os.path.join(data_path, "users_mapping.json"), "r") as f:
-		user_mapping = json.load(f)
+    with open(os.path.join(data_path, "users_mapping.json"), "r") as f:
+        user_mapping = json.load(f)
 
-	with open(os.path.join(data_path, "items_inv_mapping.json"), "r") as f:
-		item_inv_mapping = json.load(f)
+    with open(os.path.join(data_path, "items_inv_mapping.json"), "r") as f:
+        item_inv_mapping = json.load(f)
 
-	recs_mapper = generate_implicit_recs_mapper(model=model, 
-												train_matrix=interactions_train_matrix.tocsr(), 
-												top_N=10,
-												user_mapping=user_mapping,
-												item_inv_mapping=item_inv_mapping)
-	
-	train_data = pd.read_csv("/home/umbilnm/RecSysPlayground/data/interim/one_week_train.csv")
-	users = pd.Series(train_data['user_id'].unique()).to_frame(name="user_id").astype(str)
-	logger.info('inferencing')
-	recs = users['user_id'].apply(recs_mapper)
-	users['recs'] = recs
-	
+    recs_mapper = generate_implicit_recs_mapper(
+        model=model,
+        train_matrix=interactions_train_matrix.tocsr(),
+        top_N=10,
+        user_mapping=user_mapping,
+        item_inv_mapping=item_inv_mapping,
+    )
 
-	users.to_csv(path_to_save, index=False)
+    train_data = pd.read_csv("/home/umbilnm/RecSysPlayground/data/interim/one_week_train.csv")
+    users = pd.Series(train_data["user_id"].unique()).to_frame(name="user_id").astype(str)
+    logger.info("inferencing")
+    recs = users["user_id"].apply(recs_mapper)
+    users["recs"] = recs
 
-if __name__ == '__main__':
-	log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-	logging.basicConfig(level=logging.INFO, format=log_fmt)
-	project_dir = Path(__file__).resolve().parents[2]	
-	inference_implicit_model()
+    users.to_csv(path_to_save, index=False)
+
+
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    project_dir = Path(__file__).resolve().parents[2]
+    inference_implicit_model()
